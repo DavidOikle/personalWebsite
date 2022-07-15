18611d4a468ce74fc6c1186c7369cde5e4e6f64e
DavidOikle <101826302+DavidOikle@users.noreply.github.com>
2022-07-14 22:15:31 -0400

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,8 @@
+/*
+Step 1: Make a function called "computerPlay" that will randomly return "Rock" "Paper" or "Scissors"
+*/
 
+function computerPlay() {
+ let choices = ["rock", "paper", "scissors"]
+ return choices[Math.floor(Math.random() * choices.length]
+}
