4da9913cd275420852e828b8e4cf37d47b76abf4
DavidOikle <101826302+DavidOikle@users.noreply.github.com>
2022-12-04 20:30:15 -0500

    Update basicTextGame.js

--- scripts/basicTextGame.js
+++ scripts/basicTextGame.js
@@ -13,10 +13,33 @@ const handleRandomNum = () => {
 
 randomNumButton.addEventListener("click", handleRandomNum);
 
+// Player Profession chocie
 
-let playerProfession = 'warrior';
+const warriorButton = document.getElementById("warriorButton");
+const playerHealthDisplay = document.getElementById("pHealth")
+const playerProfessionDisplay = document.getElementById("pProfession")
 
-let playerHealth = playerProfession => {
+let playerProfession = '';
+
+playerProfessionDisplay.innerHTML = playerProfession;
+
+
+const playerProfessionChoice = () => {
+  playerProfession = "warrior";
+  playerProfessionDisplay.innerHTML = playerProfession;
+
+}
+
+warriorButton.addEventListener("click", playerProfessionChoice);
+
+/* Goal 2: inpliment a button that upon pressing, picks a random enemy from a list of options, and updates the enemy boxes in the HTML with the relevant information.
+
+ const randomEnemyButton = document.getElementById("randomEnemyButton");
+const randomEnemyDisplay = document.getElementById("randomEnemyDisplay");
+*/
+
+/*
+var playerHealth = playerProfession => {
   if (playerProfession === 'warrior') {
     return 10
   }
@@ -30,5 +53,10 @@ let playerHealth = playerProfession => {
 
 document.getElementById("pHealth").innerHTML = `${playerHealth(playerProfession)}`;
 document.getElementById("pProfession").innerHTML = `${playerProfession}`;
+*/
 
+// Old player profession selection stuff
+// let playerProfession = 'warrior';
 
+// Goal 1: Allow the player to choose their class based on clicking one of three buttons: "Warrior" "Rouge" "Wizard". Hide the class choice buttons upon the player choosing one. 
+// Make a function that listens for one of three button presses. When one is pressed, replace the relevant text and variables with the respective button.
