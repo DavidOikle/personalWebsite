a966b7fab1eaed72feb0a614cbff483cbf7211ad
DavidOikle <101826302+DavidOikle@users.noreply.github.com>
2022-12-06 00:20:09 -0500

    Update basicTextGame.js

--- scripts/basicTextGame.js
+++ scripts/basicTextGame.js
@@ -1,3 +1,5 @@
+/* Random number generator. */
+
 const randomNumButton = document.getElementById("randomButton");
 const randomNumDisplay = document.getElementById("randomNumberDisplay")
 
@@ -13,24 +15,46 @@ const handleRandomNum = () => {
 
 randomNumButton.addEventListener("click", handleRandomNum);
 
-// Player Profession chocie
+// Player Profession choice - updates playerProfession and playerHealth.
 
 const warriorButton = document.getElementById("warriorButton");
+const rogueButton = document.getElementById("rogueButton");
+const wizardButton = document.getElementById("wizardButton");
 const playerHealthDisplay = document.getElementById("pHealth")
 const playerProfessionDisplay = document.getElementById("pProfession")
 
+let playerHealth = null;
 let playerProfession = '';
 
+playerHealthDisplay.innerHTML = playerHealth;
 playerProfessionDisplay.innerHTML = playerProfession;
 
-
-const playerProfessionChoice = () => {
-  playerProfession = "warrior";
+const playerChoiceWarrior = () => {
+  playerProfession = "Warrior";
+  playerHealth = "10";
+  playerHealthDisplay.innerHTML = playerHealth;
   playerProfessionDisplay.innerHTML = playerProfession;
+};
 
-}
+const playerChoiceRogue = () => {
 
-warriorButton.addEventListener("click", playerProfessionChoice);
+  playerProfession = "Rogue";
+  playerHealth = "8";
+  playerHealthDisplay.innerHTML = playerHealth;
+  playerProfessionDisplay.innerHTML = playerProfession;
+};
+
+const playerChoiceWizard = () => {
+
+  playerProfession = "Wizard";
+  playerHealth = "6";
+  playerHealthDisplay.innerHTML = playerHealth;
+  playerProfessionDisplay.innerHTML = playerProfession;
+};
+
+warriorButton.addEventListener("click", playerChoiceWarrior);
+rogueButton.addEventListener("click", playerChoiceWizard);
+wizardButton.addEventListener("click", playerChoiceRogue);
 
 /* Goal 2: inpliment a button that upon pressing, picks a random enemy from a list of options, and updates the enemy boxes in the HTML with the relevant information.
 
@@ -38,25 +62,3 @@ warriorButton.addEventListener("click", playerProfessionChoice);
 const randomEnemyDisplay = document.getElementById("randomEnemyDisplay");
 */
 
-/*
-var playerHealth = playerProfession => {
-  if (playerProfession === 'warrior') {
-    return 10
-  }
-  else if (playerProfession === 'rouge') {
-    return 8
-  }
-  else if (playerProfession === 'wizard') {
-    return 6
-  }
-};
-
-document.getElementById("pHealth").innerHTML = `${playerHealth(playerProfession)}`;
-document.getElementById("pProfession").innerHTML = `${playerProfession}`;
-*/
-
-// Old player profession selection stuff
-// let playerProfession = 'warrior';
-
-// Goal 1: Allow the player to choose their class based on clicking one of three buttons: "Warrior" "Rouge" "Wizard". Hide the class choice buttons upon the player choosing one. 
-// Make a function that listens for one of three button presses. When one is pressed, replace the relevant text and variables with the respective button.
