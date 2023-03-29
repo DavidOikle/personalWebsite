33cf75dbff59c57e9d2101de42b23dbc5fe30840
DavidOikle <101826302+DavidOikle@users.noreply.github.com>
2023-03-28 22:30:25 -0400

    Update basicTextGame.js

--- scripts/basicTextGame.js
+++ scripts/basicTextGame.js
@@ -1,3 +1,8 @@
+// JS imports from other files
+
+
+import {enemyArrayExport} from "./enemyList.js"    
+
 //Variables
 
 var playerDamageRoll = 0;
@@ -8,9 +13,16 @@ var playerMaxHealth = 0;
 var playerExperience = 0;
 var playerMaxExperience = 10;
 var playerGold = 0;
-let enemyHealth = 0;
-let enemyProfession = '';
-let playerLevel = 1;
+var enemyProfession = '';
+var playerLevel = 1;
+var martial = 0;
+var guile = 0;
+var wizardry = 0
+var enemyArray = enemyArrayExport;
+var enemyHealth = null;
+var enemyMaxHealth = null;
+
+
 
 
 //DOM imports from HTML into JS
@@ -29,6 +41,11 @@ const enemyHealthDisplay = document.getElementById("eHealth")
 const enemyMaxHealthDisplay = document.getElementById("eMaxHealth")
 const enemyProfessionDisplay = document.getElementById("eProfession")
 const playerLevelDisplay = document.getElementById("pLevel")
+const playerMartialDisplay = document.getElementById("martial")
+const playerGuileDisplay = document.getElementById("guile")
+const playerWizardryDisplay = document.getElementById("wizardry")
+const fullScreenMenu = document.getElementsByClassName("fullScreenMenu")[0];
+const testButton = document.getElementById("testButton");
 
 
 
@@ -44,6 +61,70 @@ function combat() {
 }
 
 //Individual combat functions
+
+/* Parent player attack function that is filled by the other functions\
+   
+    Parent variable of "playerDamage = "Null""
+parentAttack = () => {
+  
+  enemyHealth = (enemyHealth - playerDamage);
+  enemyHealthDisplay.innerHTML = enemyHealth;
+      
+  if (enemyHealth <= 0) {
+  enemyHealth = null;
+  enemyMaxHealth = null;
+  enemyProfession = '';
+  enemyHealthDisplay.innerHTML = enemyHealth;
+  enemyMaxHealthDisplay.innerHTML = enemyMaxHealth;
+  enemyProfessionDisplay.innerHTML = enemyProfession;
+  document.getElementById("randomEnemyButton").style.visibility = "visible";
+  document.getElementById("attackButton").style.visibility = "hidden";
+  playerExperience++;
+  playerExperienceDisplay.innerHTML = playerExperience;
+
+  playerHealth = playerHealth + playerMaxHealth * Math.round(0.5);
+  if (playerHealth > playerMaxHealth) {
+    playerHealth = playerMaxHealth
+  }
+}
+ playerCombatLog.innerHTML = `You strike for ${playerDamageRoll} damage.`;
+}
+ */
+
+
+/*
+
+Repopulating the fullScreenMenu(+Middle +Bottom)
+
+var temp = () => {
+    
+}
+
+*/
+
+
+
+
+
+
+var randomEnemyFunction = () => {
+    var randomEnemy = enemyArray[Math.floor(Math.random() * enemyArray.length)];
+    enemyProfession = randomEnemy[0];
+    enemyHealth = randomEnemy[1];
+    enemyMaxHealth = randomEnemy[1];
+  
+    document.getElementById("randomEnemyButton").style.visibility = "hidden";
+    enemyHealthDisplay.innerHTML = enemyHealth;
+    enemyMaxHealthDisplay.innerHTML = enemyMaxHealth;
+    enemyProfessionDisplay.innerHTML = enemyProfession;
+    console.log("test")
+      document.getElementById("attackButton").style.visibility = "visible";
+
+}
+
+
+
+
 
 let enemyAttack = () => {
    enemyDamageRoll = Math.floor(Math.random() * 4  ) + 1;
@@ -62,19 +143,67 @@ let enemyAttack = () => {
 }
 
 let handleAttack = () => {
-  playerDamageRoll = Math.floor(Math.random() * 10  ) + 1;
-  enemyHealth = enemyHealth - playerDamageRoll;
+  playerDamageRoll = Math.floor(Math.random() * 4  ) + 1 + (martial - 10)
+  if (playerDamageRoll < 1) {
+    playerDamageRoll = 1;
+  };
+  enemyHealth = (enemyHealth - playerDamageRoll);
   enemyHealthDisplay.innerHTML = enemyHealth;
 
   // Resets the Enemy Health and Enemy Profession boxes when the enemy's health hits 0 or less.
+  
+/*   
+
+
+
+let fireBall = () => {
+  if (playerMana > 0){
+    playerMana++;
+  playerDamageRoll = Math.floor(Math.random() * 4  ) + 1 + (wizardy - 10)
+  if (playerDamageRoll < 1) {
+    playerDamageRoll = 1;
+  };
+  enemyHealth = (enemyHealth - playerDamageRoll);
+  enemyHealthDisplay.innerHTML = enemyHealth;
+}
+
+let rangedAttack = () => {
+
+    playerDamageRoll = Math.floor(Math.random() * 4  ) + 1 + (Martial - 10)
+  if (playerDamageRoll < 1) {
+    playerDamageRoll = 1;
+  };
+  enemyHealth = (enemyHealth - playerDamageRoll);
+  enemyHealthDisplay.innerHTML = enemyHealth;
+}
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
   if (enemyHealth <= 0) {
   enemyHealth = null;
+  enemyMaxHealth = null;
   enemyProfession = '';
   enemyHealthDisplay.innerHTML = enemyHealth;
+  enemyMaxHealthDisplay.innerHTML = enemyMaxHealth;
   enemyProfessionDisplay.innerHTML = enemyProfession;
-   document.getElementById("randomEnemyButton").style.visibility = "visible";
+  document.getElementById("randomEnemyButton").style.visibility = "visible";
+  document.getElementById("attackButton").style.visibility = "hidden";
   playerExperience++;
-   playerExperienceDisplay.innerHTML = playerExperience;
+  playerExperienceDisplay.innerHTML = playerExperience;
 
   playerHealth = playerHealth + playerMaxHealth * Math.round(0.5);
   if (playerHealth > playerMaxHealth) {
@@ -83,6 +212,18 @@ let handleAttack = () => {
 }
  playerCombatLog.innerHTML = `You strike for ${playerDamageRoll} damage.`;
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 let playerExperienceCheck = () => {
@@ -108,12 +249,17 @@ const playerChoiceWarrior = () => {
   playerProfession = "Warrior";
   playerHealth = 10;
   playerMaxHealth = 10;
+  martial = 14;
+  guile = 10
+  wizardry = 8;
   playerHealthDisplay.innerHTML = playerHealth;
   playerMaxHealthDisplay.innerHTML = playerMaxHealth;
   playerProfessionDisplay.innerHTML = playerProfession;
-  document.getElementById("warriorButton").style.visibility = "hidden";
-  document.getElementById("rogueButton").style.visibility = "hidden";
-  document.getElementById("wizardButton").style.visibility = "hidden";
+  playerMartialDisplay.innerHTML = martial;
+  playerGuileDisplay.innerHTML = guile;
+  playerWizardryDisplay.innerHTML = wizardry;
+  document.getElementsByClassName("fullScreenMenu")[0].style.visibility = 'hidden';
+  
 };
 
 const playerChoiceRogue = () => {
@@ -121,12 +267,16 @@ const playerChoiceRogue = () => {
   playerProfession = "Rogue";
   playerHealth = 8;
   playerMaxHealth = 8;
+  martial = 10;
+  guile = 14;
+  wizardry = 8;
   playerHealthDisplay.innerHTML = playerHealth;
-   playerMaxHealthDisplay.innerHTML = playerMaxHealth;
+  playerMaxHealthDisplay.innerHTML = playerMaxHealth;
   playerProfessionDisplay.innerHTML = playerProfession;
-  document.getElementById("warriorButton").style.visibility = "hidden";
-  document.getElementById("rogueButton").style.visibility = "hidden";
-  document.getElementById("wizardButton").style.visibility = "hidden";
+  playerMartialDisplay.innerHTML = martial;
+  playerGuileDisplay.innerHTML = guile;
+  playerWizardryDisplay.innerHTML = wizardry;
+  document.getElementsByClassName("fullScreenMenu")[0].style.visibility = 'hidden';
 };
 
 const playerChoiceWizard = () => {
@@ -134,13 +284,49 @@ const playerChoiceWizard = () => {
   playerProfession = "Wizard";
   playerHealth = 6;
   playerMaxHealth = 6;
+  martial = 8;
+  guile = 10;
+  wizardry = 14;
   playerHealthDisplay.innerHTML = playerHealth;
-   playerMaxHealthDisplay.innerHTML = playerMaxHealth;
+  playerMaxHealthDisplay.innerHTML = playerMaxHealth;
   playerProfessionDisplay.innerHTML = playerProfession;
-  document.getElementById("warriorButton").style.visibility = "hidden";
-  document.getElementById("rogueButton").style.visibility = "hidden";
-  document.getElementById("wizardButton").style.visibility = "hidden";
+  playerMartialDisplay.innerHTML = martial;
+  playerGuileDisplay.innerHTML = guile;
+  playerWizardryDisplay.innerHTML = wizardry;
+  document.getElementsByClassName("fullScreenMenu")[0].style.visibility = 'hidden';
 };
+
+
+//Generate enemy function
+
+
+
+
+
+let menuOne = () => {
+  fullScreenMenuTop.innerHTML = "Basic Web Game"
+  fullScreenMenuMiddle.innerHTML = "Hello my name is David, and welcome to the Basic Web Game!"
+  "This is a simple little project I'm using as a means of learning web development in my spare time. Be sure to check back regularly, as I continually add more to this project as I the learn the quirks and fun of HTML, CSS, JavaScript, and more!"
+  "Last Updated: March 27th 2023"
+  "To do: Add player portraits, revamp some code, and give other classes things to do other than attacking."
+  "For now though, what profession would you like to play as?"
+  fullScreenMenuBottom.innerHTML = "MenuOne Bottom Text"
+}
+
+
+
+
+
+let menuTwo = () => {
+  fullScreenMenuTop.innerHTML = "MenuOne Title"
+  fullScreenMenuMiddle.innerHTML = "MenuOne Middle Text"
+  fullScreenMenuBottom.innerHTML = "MenuOne Bottom Text"
+}
+
+
+
+
+
 
 //Event Listeners
 
@@ -148,50 +334,13 @@ warriorButton.addEventListener("click", playerChoiceWarrior);
 rogueButton.addEventListener("click", playerChoiceRogue);
 wizardButton.addEventListener("click", playerChoiceWizard);
 randomNumAttack.addEventListener("click", combat);
-
-//Generate enemy function
-
-const randomEnemyFunction = () => {
-      const randomEnemyNumber = Math.floor(Math.random() * 3) 
-          if (randomEnemyNumber == 0){
-            enemyProfession = "Bandit"
-            enemyHealth = "8";
-            enemyMaxHealth = "8";
-          }
-          else if (randomEnemyNumber == 1){
-            enemyProfession = "Brute"
-            enemyHealth = "10";
-            enemyMaxHealth = "10";
-          }
-          else if (randomEnemyNumber == 2){
-            enemyProfession = "Necromancer"
-            enemyHealth = "6";
-            enemyMaxHealth = "6";
-          }
-    document.getElementById("randomEnemyButton").style.visibility = "hidden";
-    enemyHealthDisplay.innerHTML = enemyHealth;
-    enemyMaxHealthDisplay.innerHTML = enemyMaxHealth;
-    enemyProfessionDisplay.innerHTML = enemyProfession;
-}
 randomEnemyButton.addEventListener("click", randomEnemyFunction);
+testButton.addEventListener("click", temp);
 
 
 
 
-/*
-Factory functions inital setup.
-
-function enemyFactory(enemyProfession, enemyHealth, enemyMaxHealth){
-  return {
-    enemyProfession,
-    enemyHealth,
-    enemyMaxHealth,
-  }
-}
 
 
-const newEnemy = enemyFactory('Bandit', 8, 8)
 
-
-*/
 
